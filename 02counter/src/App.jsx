d8138fe527e073a3123aead0abd1d2accc111f4d
KArtickCode <karticksau701@gmail.conm>
2025-04-18 12:53:34 +0530

    Day -> 3 : 02counter add and delete value project submit

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,12 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  let counter = 15
+
+  // const [] = useState(15)
+
+  let [counter, setCounter] = useState(15)    // here the value of the counter = 15, which is written in the useState's () 
+  // let counter = 15     // this value is not shown in UI so it is not useable here
 
   const addValue = () => {
     console.log("clicked",counter);     // in this code we update the value but not changes in UI . In this senary React comes and if we update the value in one places it changes in every where in the code. and this come done by HOOKS
-    counter = counter + 1;
+    if (counter < 20)
+      counter = counter + 1;
+    setCounter(counter)
+    console.log("after clicked",counter); 
+  }
+
+  const decreaseValue = () => {
+    console.log("clicked",counter);   //it is only for showing result in the console
+    // counter = counter - 1;
+    if(counter > 0)
+      setCounter(counter - 1)
+    console.log("after clicked",counter);
   }
 
   return (
@@ -21,7 +35,9 @@ function App() {
       onClick={addValue}
       >Add value {counter}</button>
       <br></br>
-      <button>Decrease value {counter}</button>
+      <button
+      onClick={decreaseValue}
+      >Decrease value {counter}</button>
       <p>footer: {counter}</p>
     </>
   )
